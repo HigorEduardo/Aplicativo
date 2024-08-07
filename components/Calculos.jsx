ca32cee56440bd80a06a11f67d1598701dd5cae8
HIGOR EDUARDO DA SILVA <higor_e_silva@estudante.sesisenai.org.br>
2024-08-07 10:19:05 -0300

    aaa

---
+++ components/Calculos.jsx
@@ -0,0 +1,18 @@
+function Divisao(a,b){
+    Divisao = a / b
+}
+
+function Menos(a,b){
+    Menos = a - b 
+}
+
+function Vezes(a,b){
+    Vezes = a * b
+}
+
+
+function Soma(a,b){
+    Soma = a + b
+}
+
+export default Divisao; Menos; Soma; Vezes;
